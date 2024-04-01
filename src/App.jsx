72793703a5701b2d6319760a61321416bda7cc78
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-04-02 02:52:44 +0530

    Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
+
+
+
 // import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 // import Navbar from './Navbar';
 // import LeetCode from './LeetCode';
 // import HackerRank from './HackerRank';
 // import CodingNinja from './CodingNinja';
 // import Gfg from './Gfg';
-// import Home from './Home'; // Import a component to render for the home route
+// import Footers from './Footers';
+// // Home import removed since you want Gfg as home
 
 // function App() {
 //   return (
 //     <Router>
 //       <div>
-//         <Navbar />
+//       <Navbar />
 //         <Routes>
-//           <Route path="/leetcode" element={<LeetCode/>} />
-//           <Route path="/hackerrank" element={<HackerRank/>} />
-//           <Route path="/codingninja" element={<CodingNinja/>} />
-//           <Route exact path="/Gfg" element={<Gfg/>} />
-//           {/* <Route exact path="/Home" component={Home} /> Use exact to match only the root route */}
+//           <Route path="/leetcode" element={<LeetCode />} />
+//           <Route path="/hackerrank" element={<HackerRank />} />
+//           <Route path="/codingninja" element={<CodingNinja />} />
+//           <Route path="/gfg" element={<Gfg />} />
+//           {/* Redirect root path to Gfg. No need for exact, as Routes chooses the first matching route */}
+//           <Route path="/" element={<Navigate replace to="/gfg" />} />
+//           {/* Optionally, if you want to still have a Home route, you can include it with its path */}
+//           {/* <Route path="/home" element={<Home />} /> */}
 //         </Routes>
+//       <Footers/>
+        
 //       </div>
 //     </Router>
 //   );
@@ -30,7 +39,7 @@
 
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import LeetCode from './LeetCode';
@@ -38,28 +47,25 @@ import HackerRank from './HackerRank';
 import CodingNinja from './CodingNinja';
 import Gfg from './Gfg';
 import Footers from './Footers';
-// Home import removed since you want Gfg as home
 
 function App() {
   return (
     <Router>
       <div>
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/leetcode" element={<LeetCode />} />
           <Route path="/hackerrank" element={<HackerRank />} />
           <Route path="/codingninja" element={<CodingNinja />} />
           <Route path="/gfg" element={<Gfg />} />
-          {/* Redirect root path to Gfg. No need for exact, as Routes chooses the first matching route */}
-          <Route path="/" element={<Navigate replace to="/gfg" />} />
-          {/* Optionally, if you want to still have a Home route, you can include it with its path */}
-          {/* <Route path="/home" element={<Home />} /> */}
+          {/* Redirect the root to one of the pages if desired */}
+          {/* <Route path="/" element={<Navigate replace to="/leetcode" />} /> */}
         </Routes>
-      <Footers/>
-        
+        <Footers />
       </div>
     </Router>
   );
 }
 
 export default App;
+
