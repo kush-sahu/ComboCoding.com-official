4687e0ff1ffe6f3c48a9ee6f5a2f3f523b17007e
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-10-21 19:05:17 +0530

    Update SignIn.jsx

--- src/component/SignIn.jsx
+++ src/component/SignIn.jsx
@@ -2,6 +2,13 @@ import React ,{useState}from 'react'
 import './register.css';
 import { Link } from 'react-router-dom';
 function Sign({SetEmail,SetPass,check,google,toggle}) {
+
+
+    const guesthandle=()=>{
+        SetEmail('guest@gmail.com')
+        SetPass('guestlogin')
+    }
+
    
   return (
     <div className='register'>
@@ -21,7 +28,11 @@ function Sign({SetEmail,SetPass,check,google,toggle}) {
                     <label className="form-labelr" htmlFor="password">Password</label>
                 </div>
                 <button type="submit" className="btnr">Sign In</button>
-                <center><button className="btnr " style={{ width: '40%', backgroundColor: 'orange',marginTop:'4%' }} onClick={google}>Sign In using Google </button></center>
+
+                <center>
+                    <button className="btnr " style={{ width: '40%', backgroundColor: 'red',margin:'4%' }} onClick={google}>Google </button>
+                    <button className="btnr " style={{ width: '40%', backgroundColor: 'red',margin:'4%' }} onClick={guesthandle}>Guest  </button>
+                </center>
                 <button onClick={toggle}>create account</button>
                 
             </form>
