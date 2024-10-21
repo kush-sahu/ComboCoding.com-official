93c1e952bb047c5f6a279686ca938e5924a6277d
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-10-21 19:05:43 +0530

    Update SignIn.jsx

--- src/component/SignIn.jsx
+++ src/component/SignIn.jsx
@@ -1,46 +1,40 @@
-import React ,{useState}from 'react'
-import './register.css';
-import { Link } from 'react-router-dom';
-function Sign({SetEmail,SetPass,check,google,toggle}) {
-
-
-    const guesthandle=()=>{
-        SetEmail('guest@gmail.com')
-        SetPass('guestlogin')
-    }
-
-   
+import React from 'react'
+import '../App.css'
+function User({user,signouthandle}) {
   return (
-    <div className='register'>
-    <div className="containerr">
-        <div className="cardr">
-            <h1>Sign up</h1>
-            <form onSubmit={(e)=>check(e)}>
-                <div className="form-groupr">
-                    <i className="fas fa-enveloper"></i>
-                    <input type="email" className="form-controlr" id="email" onChange={(e)=>SetEmail(e.target.value)} />
-                    <label className="form-labelr" htmlFor="email">Your Email</label>
-                </div>
-                <div className="form-groupr">
-                    <i className="fas fa-lockr"></i>
-                    <input type="password" className="form-controlr" id="password"
-                        onChange={(e)=>SetPass(e.target.value)} />
-                    <label className="form-labelr" htmlFor="password">Password</label>
-                </div>
-                <button type="submit" className="btnr">Sign In</button>
+    <>
+    <center style={{margin:'auto',padding:'13%',}}>
+    
+    
+    <h1 > <span id='user'>{user.email}</span> </h1>
 
-                <center>
-                    <button className="btnr " style={{ width: '40%', backgroundColor: 'red',margin:'4%' }} onClick={google}>Google </button>
-                    <button className="btnr " style={{ width: '40%', backgroundColor: 'red',margin:'4%' }} onClick={guesthandle}>Guest  </button>
-                </center>
-                <button onClick={toggle}>create account</button>
-                
-            </form>
-        </div>
-    </div>
 
-    </div>
+    <h1>About us</h1>
+    <div className="box">
+      <p id='loginpara'>🚀 Welcome to ComboCoding! 🌐 <a href=" https://combocoding-com.netlify.app/"> https://combocoding-com.netlify.app/</a> 
+<br />
+Dive into the ultimate learning experience with ComboCoding, your go-to destination for mastering Data Structures and Algorithms (DSA). Whether you're starting your coding journey, looking to enhance your problem-solving skills, or aiming to crack coding interviews, our platform is designed to guide you every step of the way.
+
+Why Choose ComboCoding?
+
+📚 Extensive DSA Content: Explore a rich library of DSA topics, from basic to advanced levels, curated meticulously for comprehensive learning.
+💡 Interactive Learning: Engage with interactive video tutorials and hands-on coding exercises tailored to foster deep understanding and practical skills.
+🎓 Expert Guidance: Learn from the best with content created and vetted by industry experts and seasoned educators.
+🌍 Community Support: Join a vibrant community of like-minded learners. Share insights, ask questions, and find coding buddies.
+🔗 Exclusive Resources: Access additional resources and tools exclusively available to our community members to enhance your learning journey.
+Your Path to Coding Excellence Starts Here!
+At ComboCoding, we believe in empowering learners with the knowledge and skills needed to excel in the tech industry. Our platform is more than just a learning resource; it's a gateway to unlocking your full coding potential.
+
+Begin Your Journey Today: <a href="https://rb.gy/fep1p5" >  link</a> <br />
+
+Let's code, learn, and grow together with ComboCoding. Happy coding! 🎉</p>
+   </div>
+    <button className='logout' onClick={signouthandle}>Logout</button>
+    </center>
+    <h4 style={{color:'rgb(224, 5, 100)'}}>We will be update our website for the future new requirement</h4>
+ 
+    </>
   )
 }
 
-export default Sign
+export default User
